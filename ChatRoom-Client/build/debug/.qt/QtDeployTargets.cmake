f63bc85922e4903e0f4b2760b8ce1dc7a119ee70
User <depp01030@gmail.com>
2024-09-08 18:36:17 +0800

    finish server and client first version

--- ChatRoom-Client/build/debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChatRoom-Client_FILE D:/CLearning/VSC/ChatRoom/ChatRoom-Client/build/debug/bin/ChatRoom-Client.exe)
-set(__QT_DEPLOY_TARGET_ChatRoom-Client_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChatRoom-Client_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
--- ChatRoom-Server/build/debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChatRoom-Server_FILE D:/CLearning/VSC/ChatRoom/ChatRoom-Server/build/debug/bin/ChatRoom-Server.exe)
-set(__QT_DEPLOY_TARGET_ChatRoom-Server_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChatRoom-Server_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
